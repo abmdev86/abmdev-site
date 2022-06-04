4d634ade6ed53b405deeea5715d37c4927360e94
Auroiah Morgan <amwebdev@outlook.com>
2022-06-03 22:07:09 -0500

    Adds BlazorBootstrap
    Adds BlazorBootstrap. update the csproj files and Program.cs file for bootstrap

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,3 +1,4 @@
+using BlazorBootstrap;
 using BlazorBasic;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,5 +8,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBlazorBootstrap();
 
 await builder.Build().RunAsync();
